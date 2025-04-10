290c7a2a15170c8bfc2eef75ee5da7f6f3f990b2
GioFic09 <157527157+GioFic09@users.noreply.github.com>
2025-04-10 00:23:09 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,11 @@
+let giovanna_nota_av1 = 8;
+let giovanna_nota_av2 = 9.6;
+let giovanna_media = (giovanna_nota_av1 + giovanna_nota_av2) / 2;
+
+console.log("Média Giovanna " + giovanna_media);
+let notas_giovanna = [8,9.6,];
+console.log("AV1: "+notas_giovanna[0]);
+console.log("AV2: "+notas_giovanna[1]);
+notas_giovanna[2] = (notas_giovanna[0] + notas_giovanna[1]) / 2;
+console.log("Média: "+notas_giovanna[2]);
+console.log("Todas: "+notas_giovanna);
